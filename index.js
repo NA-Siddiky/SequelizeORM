7ecbd1210e4cdd07c873351c1e43d67fc86a3cb6
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-30 00:43:19 +0600

    initilize server config

--- index.js
+++ index.js
@@ -2,9 +2,37 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const port = 5050;
+
+//body parser connection
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const sequelize = new Sequelize('to_do_sql', 'root', '12345678', {
+    host: 'localhost',
+    // port: 3306,
+    dialect: 'mssql'
+});
+
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('successfully connected sql');
+//     })
+//     .catch((err) => {
+//         console.log(err, 'this has a error');
+//     })
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 
 app.get("/", (req, res) => {
     res.send("this is working server is connected");
