d35ca5e6dbb710465d6e52f3415453d67e2d0bbd
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-30 02:19:08 +0600

    Posting data to table in db

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //import 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const { Sequelize } = require('sequelize');
 
 const port = 5050;
 
+const app = express();
 //body parser connection
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +16,21 @@ const sequelize = new Sequelize('to_do_sql', 'root', '12345678', {
     dialect: 'mysql'
 });
 
-//creating db table
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
+
+
+app.get("/", (req, res) => {
+    res.send("this is working server is connected");
+})
+
+//creating db table(create-C)
 const blog_table = sequelize.define(
     "blog_table",
     {
@@ -28,18 +42,16 @@ const blog_table = sequelize.define(
 
 blog_table.sync({ force: true });
 
-
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-})();
-
-app.get("/", (req, res) => {
-    res.send("this is working server is connected");
+//post data on table(post)
+app.post('/', async (req, res) => {
+    const title = req.body.title;
+    const desc = req.body.desc;
+    const saveBlog = blog_table.build({
+        title,
+        desc,
+    })
+    await saveBlog.save();
+    res.send("data post successfully");
 })
 
 app.listen(port, () => {
